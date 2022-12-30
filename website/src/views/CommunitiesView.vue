357c635ae57742af62f9f90ba2ac2b48819dbf14
Lawrence Lee <lee.lawrenceh@gmail.com>
2022-12-30 01:16:36 -0800

    Add breadcrumbs to CommunitiesView

--- website/src/views/CommunitiesView.vue
+++ website/src/views/CommunitiesView.vue
@@ -1,10 +1,20 @@
 <script lang="ts" setup>
 import CommunitiesList from "@/components/CommunitiesList.vue";
 import { useRequireLogin } from "@/composables/useRequireLogin";
+import BreadcrumbTrail from "@/components/BreadcrumbTrail.vue";
+import type Breadcrumb from "@/services/breadcrumb";
 
 useRequireLogin();
+
+const crumbs: Breadcrumb[] = [
+  {
+    name: "Communities",
+    path: "/communities",
+  },
+];
 </script>
 
 <template>
+  <BreadcrumbTrail :crumbs="crumbs"></BreadcrumbTrail>
   <CommunitiesList></CommunitiesList>
 </template>
